3116e1b8eb357d305cde7e0d1c604a1447a1151f
Alex Hordynska <ahordynska@gmail.com>
2019-08-05 18:43:53 +0200

    Header and order for mobile finished

--- src/index.js
+++ src/index.js
@@ -29,92 +29,99 @@ class Checkout extends Component {
     zipCode: '',
     city: '',
     state: '',
-    country: '',
     cardNumber: '',
     cardHolder: '',
     cvc: ''
   };
-  handleName = e => {
+  handleChange = e => {
     this.setState({
-      name: e.target.value
+      [e.target.name]: e.target.value
     });
   };
   render() {
     return (
       <section>
-        <h2>CHECKOUT</h2>
         <h3>DELIVERY ADDRESS</h3>
         <form>
           <label>
             FIRST NAME
             <input
               type='text'
+              name='name'
               value={this.state.name}
-              onChange={this.handleName}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             LAST NAME
             <input
               type='text'
+              name='surname'
               value={this.state.surname}
-              onChange={this.handleSurname}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             E-MAIL
             <input
               type='email'
+              name='email'
               value={this.state.email}
-              onChange={this.handleEmail}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             PHONE
             <input
               type='number'
+              name='phone'
               value={this.state.phone}
-              onChange={this.handlePhone}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             ADDRESS
             <input
               type='text'
+              name='address'
               value={this.state.address}
-              onChange={this.handleAddress}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             ADDRESS CONT...
             <input
               type='text'
+              name='addressCont'
               value={this.state.addressCont}
-              onChange={this.handleAddressCont}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             ZIP CODE
             <input
               type='text'
+              name='zipCode'
               value={this.state.zipCode}
-              onChange={this.handleZipCode}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             CITY
             <input
               type='text'
+              name='city'
               value={this.state.city}
-              onChange={this.handleCity}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             STATE
             <input
               type='text'
+              name='state'
               value={this.state.state}
-              onChange={this.handleState}
+              onChange={this.handleChange}
             />
           </label>
           <label>
@@ -131,24 +138,27 @@ class Checkout extends Component {
             CARD NUMBER
             <input
               type='number'
+              name='cardNumber'
               value={this.state.cardNumber}
-              onChange={this.handleCardNumber}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             CARD HOLDER
             <input
               type='text'
+              name='cardHolder'
               value={this.state.cardHolder}
-              onChange={this.handleCardHolder}
+              onChange={this.handleChange}
             />
           </label>
           <label>
             CVC
             <input
               type='number'
+              name='cvc'
               value={this.state.cvc}
-              onChange={this.handleCvc}
+              onChange={this.handleChange}
             />
           </label>
           <button type='submit' onClick={this.handleSubmit}>
@@ -160,11 +170,56 @@ class Checkout extends Component {
   }
 }
 
+const Order = () => {
+  return (
+    <>
+      <h3>ORDER</h3>
+      <div className='order'>
+        <div className='order-product'>
+          <div className='product-info'>
+            <h4>ACME PRODUCT</h4>
+            <div className='product-info_qty'>
+              <span>Qty</span>
+              <span>1</span>
+            </div>
+            <div className='product-info_price'>
+              <span>Price</span>
+              <span>$00</span>
+            </div>
+          </div>
+          <img alt='product' src='https://fakeimg.pl/70/683a8a/' />
+        </div>
+        <hr />
+        <div className='order-sum'>
+          <div>
+            <span>Subtotal</span>
+            <span>$00,000</span>
+          </div>
+          <div>
+            <span>Tax</span>
+            <span>$00</span>
+          </div>
+          <div>
+            <span>Shipping</span>
+            <span>$00</span>
+          </div>
+          <div>
+            <span>Total</span>
+            <span>$00,000</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 class App extends Component {
   render() {
     return (
       <>
         <Header />
+        <h2>CHECKOUT</h2>
+        <Order />
         <Checkout />
       </>
     );
